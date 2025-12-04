9202cfdee94fde5506367d73005ea4cf17170001
viteshbava <82492234+viteshbava@users.noreply.github.com>
2025-12-04 17:57:01 +1100

    Adjust style of contact success message

--- src/components/Forms/ContactForm/ContactForm.tsx
+++ src/components/Forms/ContactForm/ContactForm.tsx
@@ -194,8 +194,8 @@ const ContactForm = ({ className = '', settings }: ContactFormProps) => {
       )}
 
       {status === 'success' && (
-        <div className='bg-brand-white rounded-lg shadow-sm p-6 mt-4 text-center'>
-          <p className='text-h4 mb-2'>{successHeading}</p>
+        <div className='bg-black rounded-lg shadow-sm p-6 mt-4 text-center'>
+          <p className='text-h4 mb-2 text-brand-secondary'>{successHeading}</p>
           <p className='text-body-base mb-4'>{successMessage}</p>
         </div>
       )}
