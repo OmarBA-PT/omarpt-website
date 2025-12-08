c55890dc486af95cb33613a2c8c76df7a3c1b241
viteshbava <82492234+viteshbava@users.noreply.github.com>
2025-12-08 20:04:21 +1100

    Add animatino to google map

--- src/components/_blocks/GoogleMap.tsx
+++ src/components/_blocks/GoogleMap.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { stegaClean } from 'next-sanity';
 import type { GoogleMap as GoogleMapType } from '@/sanity/types';
 import { maxCardWidth } from '@/utils/spacingConstants';
+import AnimateIn from '../UI/AnimateIn';
 
 interface GoogleMapProps extends GoogleMapType {
   className?: string;
@@ -29,23 +30,27 @@ const GoogleMap: React.FC<GoogleMapProps> = ({ embedCode, className = '' }) => {
 
   if (!embedUrl) {
     return (
-      <div className={`${className} p-4 border border-red-200 rounded-lg bg-red-50 ${maxCardWidth} mx-auto`}>
+      <div
+        className={`${className} p-4 border border-red-200 rounded-lg bg-red-50 ${maxCardWidth} mx-auto`}>
         <p className='text-red-600'>Invalid Google Maps embed code provided</p>
       </div>
     );
   }
 
   return (
-    <div className={`${className} relative ${maxCardWidth} mx-auto w-full aspect-4/3 overflow-hidden`}>
-      <iframe
-        className='absolute inset-0 w-full h-full rounded-2xl lg:rounded-[1.25rem] border-0'
-        src={embedUrl}
-        title='Google Map'
-        loading='lazy'
-        referrerPolicy='no-referrer-when-downgrade'
-        allowFullScreen
-      />
-    </div>
+    <AnimateIn animation='scaleUp' trigger='scroll' duration={800} threshold={0.5}>
+      <div
+        className={`${className} relative ${maxCardWidth} mx-auto w-full aspect-4/3 overflow-hidden`}>
+        <iframe
+          className='absolute inset-0 w-full h-full rounded-2xl lg:rounded-[1.25rem] border-0'
+          src={embedUrl}
+          title='Google Map'
+          loading='lazy'
+          referrerPolicy='no-referrer-when-downgrade'
+          allowFullScreen
+        />
+      </div>
+    </AnimateIn>
   );
 };
 
